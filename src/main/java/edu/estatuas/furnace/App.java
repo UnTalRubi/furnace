c0ad66d3d6a43b93760f712746f603d0a1b97148
UnTalRubi <untalrubi@gmail.com>
2025-04-23 21:00:22 +0200

    feat: Create Jedi Class

--- src/main/java/edu/estatuas/furnace/App.java
+++ src/main/java/edu/estatuas/furnace/App.java
@@ -3,6 +3,7 @@ package edu.estatuas.furnace;
 import edu.estatuas.furnace.devices.GasHeater;
 import edu.estatuas.furnace.devices.Regulator;
 import edu.estatuas.furnace.devices.Sensor;
+import edu.estatuas.furnace.devices.Jedi;
 import edu.estatuas.furnace.interfaces.Heater;
 import edu.estatuas.furnace.interfaces.Thermometer;
 import edu.estatuas.furnace.miscelaneus.RoomTemperature;
@@ -21,5 +22,14 @@ public class App {
 
         System.out.println("Starting...");
         regulator.regulate(thermometer, heater, MIN_TEMP, MAX_TEMP, temperature);
+
+        temperature = RoomTemperature.getInstance();
+        Jedi yoda = new Jedi("Yoda");
+        System.out.println("\nYoda shows up: ");
+        regulator.regulate(thermometer, yoda, MIN_TEMP, MAX_TEMP, temperature);
+        yoda.speak();
+
+        Heater grogu = new Jedi("Grogu");
+        ((Jedi) grogu).speak();
     }
 }
---
+++ src/main/java/edu/estatuas/furnace/devices/Jedi.java
@@ -0,0 +1,45 @@
+package edu.estatuas.furnace.devices;
+
+import edu.estatuas.furnace.interfaces.Heater;
+import edu.estatuas.furnace.miscelaneus.RoomTemperature;
+
+public class Jedi implements Heater {
+
+    private String name;
+
+    public Jedi(String name) {
+        this.name = name;
+    }
+
+    public void speak() {
+        System.out.println("\n"
+                + "\t  __.-._   \n"
+                + "\t  '-._\"7' \n"
+                + "\t   /'.-c   \n"
+                + "\t   |  /T   \n"
+                + "\t  _)_/LI   \n"
+                + "\nDo or do not. There is no try "
+                + "\n" + this.name);
+    }
+
+    private double lightSaber() {
+        final double lightSaberTemp = 1400.d;
+        return lightSaberTemp;
+    }
+
+    private void forcePersuasion(Heater heater, RoomTemperature temperature) {
+        temperature.incrementTemperature(-lightSaber());
+        heater.disengage(temperature);
+    }
+
+    @Override
+    public void engage(RoomTemperature temperature) {
+        temperature.incrementTemperature(lightSaber());
+    }
+
+    @Override
+    public void disengage(RoomTemperature temperature) {
+        Heater heater = new GasHeater();
+        this.forcePersuasion(heater, temperature);
+    }
+}
